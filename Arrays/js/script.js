7cb8da6c3927713b50d257c8c89dd075ad01acce
tylerlidster <tyler.lidster@me.com>
2014-07-15 17:58:30 -0500

    Arrays
    Learned to use arrays and access items within an array

---
+++ Arrays/js/script.js
@@ -0,0 +1,10 @@
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3,4..
+
+
+muppetNames[1] = "Rizzo"; //change index #1 to Rizzo
+muppetNames[2] = "Fozzy";
+
+console.log(muppetNames[1]); //lets me see whats in the index #1
+
+
+

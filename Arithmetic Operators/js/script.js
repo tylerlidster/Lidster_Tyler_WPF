6858c52e8b52f83179722b0e4648a742f05cbd34
tylerlidster <tyler.lidster@me.com>
2014-07-17 14:20:18 -0500

    Added the modulo operator
    The modulo operator gives you the remainder after using division. 32 %
    2 gives you a remainder of 2.

--- Arithmetic Operators/js/script.js
+++ Arithmetic Operators/js/script.js
@@ -2,12 +2,26 @@
 
 //expression to find the area of a triangle
 //area of the triangle is half of the width times the height
-var width = 4;
-var height = 5;
-var area = width * height/2; //multiplication/division operator
-console.log(area);
+//var width = 4;
+//var height = 5;
+//var area = width * height/2; //multiplication/division operator
+//console.log(area);
 
 //multiplication operator is *
 //division operator is /
+
+//Modulo Operator
+//used to find the remainder of the division
+
+//var remainder = 32 / 10;
+//console.log(remainder);
+//The result will be 3.2
+
+//Modulo operator is %
+//Use this to get the remainder
+//var remainder = 32 % 10;
+//console.log(remainder);
+//The result/remainder is 2
+
 
 

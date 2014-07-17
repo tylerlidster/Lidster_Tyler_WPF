7a9f8dd9cd2345262e662a26fff3bce1d355a992
tylerlidster <tyler.lidster@me.com>
2014-07-17 14:02:40 -0500

    Calculate age
    Calculate age after defining variables and using an arithmetic operator

---
+++ Expressions - Age Example/js/script.js
@@ -0,0 +1,10 @@
+//Expressions - Age Example
+
+//Use an expression to calculate our age
+
+var yearBorn = 1989; //declare variable
+var age = 2014 - yearBorn; //uses an arithmetic operator
+//console.log(age);
+alert(age);
+
+//Calculates age by defining variables and using an arithmetic operator

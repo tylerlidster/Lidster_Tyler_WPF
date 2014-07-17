92775f6f54b5883b97b1b3ac795c02caa4f25710
tylerlidster <tyler.lidster@me.com>
2014-07-16 20:12:00 -0500

    Calculated user age
    Calculated user age

--- GoToTrainingWeek2/js/script.js
+++ GoToTrainingWeek2/js/script.js
@@ -3,4 +3,43 @@ Tyler Lidster
 WPF 1407 Section 01
 GoTo Training Week 2
  */
-alert("Testing 1,2,3");
+
+//alert("Testing 1,2,3");
+
+//Create a js file that will tell us how old we are
+
+
+//Create a variable to hold the persons name
+//Do not use quotes or it will displayed what is typed within them
+//Text String with quotes - Variable without quotes
+var name = prompt("Please Type In Your Name");
+
+//Console.log out that name
+console.log(name);
+
+//Alert the user and tell him hi
+alert("Welcome "+name+"! We have a few more questions for you");
+
+//Create the variable and prompt user for year of birth
+var yearBorn = prompt("What Year Were You Born?");
+console.log(yearBorn);
+
+//Create a variable for the current year
+var currentYear = 2014;
+
+
+//Calculate the persons age - save that calculation as a variable
+var age = currentYear - yearBorn;
+console.log(age);
+
+//Let's tell the user how old they are
+alert(name + " You Are "+age+" years old.");
+console.log(name + " You Are "+age+" years old.");
+
+//Let's make it more complex
+
+//How old you will be in the future
+
+//Prompt user for how many years in the future
+var yearsMore = prompt("How Many Years In The Future Would You Like?");
+console.log(yearsMore);

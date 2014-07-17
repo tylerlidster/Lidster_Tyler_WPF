ce7d66e606519705f9587b804d803258025f20db
tylerlidster <tyler.lidster@me.com>
2014-07-17 17:42:52 -0500

    Assignment Operators
    equal sign is the basic assignment operators but there are many more
    such as, =+ // -= // *= // /= // and increment and decrement operators
    that add or subtract 1 from the value using either the ++ ; or two
    minus signs with no space before the ;

---
+++ Assignment Operators/js/script.js
@@ -0,0 +1,63 @@
+//Assignment Operators
+//An expression with assignment
+//For creating arithmetic expressions
+
+//equal sign operation
+
+//var a = 3;
+//a + 4; //does not store the + 4
+//console.log(a);
+//will display the number 3
+
+
+
+//to change a we need to assign the expression to a
+//var a = 3;
+//a = a + 4; //storage container always goes on the left
+//console.log(a);
+//adds 3 = 4 and displays the number 7
+
+
+
+//The =+ operator
+//var a = 3;
+//a += 4;
+//console.log(a);
+
+//same as above
+// just uses an assignment operator that combines those two functions
+
+
+//to multiply use *=
+
+// same as above but it would be a *= 4;
+//which would multiply 4 * 3 and display the result
+
+//to divide use /=
+
+// a /= 4;
+//divides 3 / 4 and displays result
+
+//to subtract use -= operator
+
+
+//increment operators
+//only works with the number 1
+
+var a = 3;
+a ++ ; //is the same as a = a + 1 //a =+ 1
+console.log(a);
+//which gives you 3 + 1 and displays as 4
+
+
+//decrement operator does the same just subtracts 1
+a--; //no space //the same as //a = a - 1 // a -+1
+//which gives you 3 - 1 and displays as 2
+
+
+
+
+
+
+
+

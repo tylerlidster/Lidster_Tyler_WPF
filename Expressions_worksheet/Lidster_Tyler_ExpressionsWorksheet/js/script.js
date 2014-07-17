e56965b0f3bf6d727682d15cdad87b9ec100fcc2
tylerlidster <tyler.lidster@me.com>
2014-07-17 18:51:14 -0500

    Lidster_Tyler_Dog Years
    Update to last post, simplified code

---
+++ Expressions_worksheet/Lidster_Tyler_ExpressionsWorksheet/js/script.js
@@ -0,0 +1,13 @@
+/*
+ Tyler Lidster
+ WPF 1407 Section 01
+Dog Years
+ */
+
+
+var humanYears = 1;//User can input human years
+var dogYears = 7;//User can put in dog years
+var dogAge = humanYears *= 7;//Gives Sparky's age in dog years
+var humanAge = dogYears /= 7;//Gives Sparky's age in human years
+console.log("Sparky is "+humanAge+" years old which is "+dogAge+" in dog years");
+//Outputs information to the user. Allows user to input either an age in either dog years or human years and get a result

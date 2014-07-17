cda426b94a9ab3cc528700a2722c4383c6ad7b5f
tylerlidster <tyler.lidster@me.com>
2014-07-17 13:50:15 -0500

    Using an assignment operator
    Set up and defined our variable of a, used an assignment operator that
    adds to a and stores the result.
    
    Set up and defined a variable of b, stored the value of a + 3 in the
    variable b and used console.log to output the result

---
+++ Basic Expressions/js/script.js
@@ -0,0 +1,24 @@
+//Expressions
+
+//Sets up our variable a and defines it with 2
+//Write an expression that adds 3 to our variable
+//Addition operator is a +
+//Use an assignment operator to add to a and store
+
+//var a = 2;
+//a = a + 3; //adds 3 to a and stores it
+//console.log(a); //will output 5 by adding 3 to our a variable
+
+var a = 2;
+var b;
+b = a + 3; //storing the value of a + 3 in b
+console.log(b);
+
+
+
+
+
+
+
+
+

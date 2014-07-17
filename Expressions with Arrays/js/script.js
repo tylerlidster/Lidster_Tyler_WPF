f2e296617f2ecf705f9050ac25b9923d98d2cd5a
tylerlidster <tyler.lidster@me.com>
2014-07-17 14:43:07 -0500

    creating an array
    Creating an array to add and total the number of oranges in a bin

---
+++ Expressions with Arrays/js/script.js
@@ -0,0 +1,10 @@
+// Expressions with Arrays
+
+var orangeBins = [234, 567, 883];
+
+//Identify each bin number in the array
+//Write the name of the array [] then the number in the array we want to access
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
+
+//Machine adds all items in the array and gives the total

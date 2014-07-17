f951182f04cab8d75e0597ac6b6b6f4a850de671
tylerlidster <tyler.lidster@me.com>
2014-07-17 16:51:17 -0500

    learned concatenating strings
    combining two or more strings which uses the plus sign

---
+++ Concatenating Strings/js/script.js
@@ -0,0 +1,23 @@
+//Concatenating Strings
+
+//var firstName = "Kermit";
+//var lastName = "The Frog";
+//var fullName = firstName + " " + lastName;
+//space in between parantheses
+//console.log(fullName);
+
+
+//This creates a concatenating string to form the string 67
+var a = "6";
+var b = "7";
+
+var result = a + b;
+console.log(result);
+
+
+//This will add the numbers to 13  and will no longer be a string
+var a = 6;
+var b = 7;
+
+var result = a + b;
+console.log(result);

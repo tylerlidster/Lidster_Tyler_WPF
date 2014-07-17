99a73f9d7ed8a450c1838103063c7350671d4844
tylerlidster <tyler.lidster@me.com>
2014-07-17 14:34:37 -0500

    Using order of operations
    Using order of operations to find the average of quiz scores and
    perimeter

---
+++ Order of Operations/js/script.js
@@ -0,0 +1,21 @@
+// Order of Operations
+//PEMDAS
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+
+//the average is the sum of the items divided by the number of items
+//Using PEMDAS the division must happen before the addition
+//To do the addition first we need to use parentheses
+//The division then goes outside the parentheses
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+//console.log(average);
+
+//Perimeter
+var length = 7;
+var width = 6;
+var perimeter = length *  2 + width * 2;
+console.log(perimeter);

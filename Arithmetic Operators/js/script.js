a97adcad9a256e4eaddb9ab1d0c57b95907136c0
tylerlidster <tyler.lidster@me.com>
2014-07-17 14:12:05 -0500

    finding the area of a triangle
    using multiplication and division arithmetic operators to find the area
    of a triangle

---
+++ Arithmetic Operators/js/script.js
@@ -0,0 +1,13 @@
+//Arithmetic Operators
+
+//expression to find the area of a triangle
+//area of the triangle is half of the width times the height
+var width = 4;
+var height = 5;
+var area = width * height/2; //multiplication/division operator
+console.log(area);
+
+//multiplication operator is *
+//division operator is /
+
+

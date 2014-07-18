629220c4a8da5f2cf2418c3f1626746dade02efb
tylerlidster <tyler.lidster@me.com>
2014-07-17 20:45:40 -0500

    Create interchangeable heading for FullSail
    Used various prompts to allow the user to input their information,
    allowing them to ultimately be able to create their Full Sail heading
    using the information they input.

---
+++ Lidster_Tyler_Expressions/Create Personal Heading for FullSail/js/script.js
@@ -0,0 +1,30 @@
+/*
+ Tyler Lidster
+ WPF 1407 Section 01
+ Expressions - Wacky
+ */
+
+
+
+//Creates first line of heading using fullName
+var firstName = prompt("Please enter your first name.");
+var lastName = prompt("Please enter your last name");
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//Creates the second line of heading using classLine
+var className = prompt("Please enter the name of your class.");
+var classNumber = prompt("Please enter your class number.");
+var sectionNumber = prompt("Please enter your section number.");
+var classLine = className + " " + classNumber + " Section " + sectionNumber;
+console.log(classLine);
+
+//Creates the third line of heading using assignmentName
+var assignmentName = prompt ("Please enter the name of your assignment.")
+console.log(assignmentName);
+
+
+
+
+
+

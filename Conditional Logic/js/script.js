ffa56628d8f42dfd4b81a18482316651308c40a5
tylerlidster <tyler.lidster@me.com>
2014-07-22 18:00:37 -0500

    Conditionals
    Set up the basic structure of a conditional

---
+++ Conditional Logic/js/script.js
@@ -0,0 +1,15 @@
+//Conditional Logic
+
+var oldEnough = true;
+
+//if the child is old enough print to the console "You can ride"
+
+//basic structure of a conditional statement
+//the condition goes inside the parentheses
+//the code block goes inside the curly braces - if true
+
+if(oldEnough){
+    console.log("You Can Ride The Coaster");
+}
+
+

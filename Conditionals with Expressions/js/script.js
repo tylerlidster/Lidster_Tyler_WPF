4e4de54b80e8505d90b44a89f3492367f28c4ff6
tylerlidster <tyler.lidster@me.com>
2014-07-22 18:58:36 -0500

    Conditionals and Expressions
    Using conditionals and expressions together

---
+++ Conditionals with Expressions/js/script.js
@@ -0,0 +1,19 @@
+//Conditional Logic
+
+var kidHeight = 47;
+var minHeight = 48;
+
+//if the kid is old enough, print to the console You Can Ride
+//if the kid is over 48 inches in height
+
+if(kidHeight + 2 > minHeight){
+    //code performed if condition is true
+    console.log("You Can Ride The Coaster!");
+}
+
+//  Alternate Way
+//  add
+//  var sneakerLift = 2;
+//  if(kidHeight + sneakerLift > minHeight){
+//  console.log("You can ride the coaster!")
+//  }

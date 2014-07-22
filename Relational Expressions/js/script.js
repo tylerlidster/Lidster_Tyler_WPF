6b27fe5395f9490b5f18aab0abfd1988a591095c
tylerlidster <tyler.lidster@me.com>
2014-07-22 18:49:11 -0500

    Relational Expressions
    Using variables and conditionals together

---
+++ Relational Expressions/js/script.js
@@ -0,0 +1,19 @@
+//Relational Expressions
+//Machine checks the relationship between two
+//Conditional Logic
+
+var kidHeight = 50;
+var minHeight = 48;
+
+//basic structure of a conditional statement
+//the condition goes inside the parentheses
+//the code block goes inside the curly braces - if true
+
+//if the child is old enough print to the console "You can ride"
+//if the kid is over 48 inches in height
+
+//Will be true if kidHeight is greater than 48
+if(kidHeight > minHeight){
+}
+//Code performed if condition is true
+    console.log("You Can Ride The Coaster");

af9a53e63f7005a745a105ae6f92885dccb492ce
tylerlidster <tyler.lidster@me.com>
2014-07-23 18:41:32 -0500

    Relational Operators
    covered greater than, less than, greater than and equal to, less than
    and equal to. Equality and strict equality operators.

---
+++ Relational Operators/js/script.js
@@ -0,0 +1,50 @@
+//Relational Operators
+//Comparing 2 variables or values
+
+//Greater Than >
+//Less Than <
+//Greater than or equal to >=
+//Less than or equal to <=
+
+//They compare two values or variables (only 2!)
+
+//The comparison results in TRUE or FALSE
+
+
+//EQUALITY OPERATORS
+
+//Equality is ==
+//Strict Equality is ===
+//Inequality is !=
+
+
+//Never use a space between an operator!!!
+
+//COMPARING VALUES
+//5 < 5 is false
+//5 <= 5 is true
+//Statements that are logically the same
+//a > b     b < a
+
+//Equality == compares two values
+//Assignment = stores values
+
+// a==b means a is the same as b
+//a=b - means - a stores the value of b
+
+//STRICT EQUALITY uses the === operator
+//"the same value and the same type"
+//the same type means
+// - 6 number
+// - "6" string
+// - 6 == "6"
+// - 6 === "6" does not work/is false
+
+//USE STRICT EQUALITY OVER EQUALITY
+// === IS BETTER THAN ==
+
+//Inequality uses the != operator
+//which is NOT the same as a!=b
+
+
+

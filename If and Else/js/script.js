098cbb259912a3e95579666ce08e060ca684b243
tylerlidster <tyler.lidster@me.com>
2014-07-22 21:27:02 -0500

    Using IF and ELSE
    Choosing between two blocks of code, if this then this or this, also
    using “else” instead of a second “if” statement

---
+++ If and Else/js/script.js
@@ -0,0 +1,31 @@
+//If and Else
+//Choosing between two sections of code
+
+var kidHeight = 47;
+var minHeight = 48;
+
+
+//if the child is old enough, print to the console "you can ride"
+//if the kid is over 48 inches in height
+
+
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
+/*
+if(kidHeight <= minHeight){
+    console.log("Sorry kid you've got some growing to do!");
+}
+*/
+//If the kidHeight and minHeight are both 48 nothing will
+// show because we are using the greater than and less than as opposed
+//to using the <= or >=
+
+
+
+//To combine the if statements use else
+//code then performed if condition is false
+}else{
+    console.log("sorry kid, you've got some growing to do")
+}

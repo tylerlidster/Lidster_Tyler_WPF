5c789044a3a80a3d35f3397f757d9db0a465092a
tylerlidster <tyler.lidster@me.com>
2014-07-23 21:27:32 -0500

    Logical Operators
    Using logical operators to compare values

---
+++ Logical Operators/js/script.js
@@ -0,0 +1,13 @@
+//Conditional Logic - Logical Operators
+
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
+
+//if the price of the phone is less than our budget AND if our paycheck is over 300
+if(iPhonePrice < budget && paycheck > 300){
+    console.log("We can bu the iPhone!");
+}else{
+    console.log("No phone for you!");
+}
+

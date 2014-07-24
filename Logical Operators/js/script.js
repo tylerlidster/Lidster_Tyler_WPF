d722620da79ad50877b0f6a12f20368479d39ccf
tylerlidster <tyler.lidster@me.com>
2014-07-24 12:21:47 -0500

    Logical Operators
    What logical operators are, how they are represented and how and when
    to use them - notes are accompanied in JS Conditionals folder on
    desktop for reference

--- Logical Operators/js/script.js
+++ Logical Operators/js/script.js
@@ -1,13 +1,23 @@
 //Conditional Logic - Logical Operators
-
+/*
 var budget = 300;
 var iPhonePrice = 199.99;
 var paycheck = 200;
 
 //if the price of the phone is less than our budget AND if our paycheck is over 300
 if(iPhonePrice < budget && paycheck > 300){
-    console.log("We can bu the iPhone!");
+    console.log("We can buy the iPhone!");
 }else{
     console.log("No phone for you!");
 }
+*/
 
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
+
+if(iPhonePrice < budget || wonLottery){
+    console.log("We can buy the iPhone!");
+}else{
+    console.log("No phone for you!");
+}

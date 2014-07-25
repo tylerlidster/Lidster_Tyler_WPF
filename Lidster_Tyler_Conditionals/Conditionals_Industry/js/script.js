078d7b8e9c19d3026791c503f71c4cbbcb63e2b9
tylerlidster <tyler.lidster@me.com>
2014-07-24 20:53:26 -0500

    Conditionals_Industry
    Week 3 assignment - helps the user to know whether to discount a hotel
    rate based off the occupancy at that time

--- Lidster_Tyler_Conditionals/Conditionals_Industry/js/script.js
+++ Lidster_Tyler_Conditionals/Conditionals_Industry/js/script.js
@@ -4,4 +4,15 @@ WPF 0714 Section 01
 Conditionals_Industry
 */
 
-
+//Sets a value of 50 as the hotels base occupancy to decide on which rate to sell
+var occupants = 50;
+//prompts the user for occupancy number
+var occupancy = prompt("What is our occupancy tonight?");
+console.log(occupancy);
+//if the hotels occupancy is greater than 50 sell each room at 150
+//if the hotels occupancy is less than 50 sell at a discount
+if(occupancy > occupants){
+    alert("Sell each room at a rate of $150.");
+}else{
+    alert("Sell each room at a discounted rate of $100");
+}

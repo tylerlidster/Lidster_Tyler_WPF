af83edde253e8b440a7e44ee4ad80da2b1197eec
tylerlidster <tyler.lidster@me.com>
2014-07-31 12:33:26 -0700

    Arguments and Parameters
    Arguments and Parameters

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -24,3 +24,19 @@ function calcArea(){
 //Makes the the code reusable again and again
 
 
+/*Arguments and Parameters
+
+calcArea(30, 20);
+
+function calcArea(w, h){ //w=30, h=20
+    var area = w * h;
+    console.log(area);
+}
+*/
+
+function dogYears(){
+    var age = 4;
+    var dogYears = age * 7;
+    console.log("Sparky is " +dogYears + " years old.");
+}
+dogYears();

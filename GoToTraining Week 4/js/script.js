e5d24ab7a022d7791984375d28ae2248c62abb6d
tylerlidster <tyler.lidster@me.com>
2014-07-31 11:07:16 -0700

    Set up and validate max number prompt in while loop
    Set up and validate max number prompt in while loop

--- GoToTraining Week 4/js/script.js
+++ GoToTraining Week 4/js/script.js
@@ -27,10 +27,34 @@ while(isNaN(min)){
 }
 */
 //combine Validation into 1 while loop
-//Combines both examples from abover
+//Combines both examples from above
 //Tests for both an empty string and not a number
 //Use OR not AND - can't be a number and an empty string at once
 while(isNaN(min) || min===""){
     //Re-Prompt the user
     min = prompt("Please type in a min number:");
 }
+/*
+//Validating words
+//Ask user yes or no
+// "yes" is not the same as "Yes" or "YES"
+var yesOrNo = prompt("Please type in yes or no ONLY!");
+
+while(yesOrNo==="yes" && yesOrNo!="no" && yesOrNo!="Yes"){
+    yesOrNo = prompt("Please type in yes or no ONLY!");
+
+}
+*/
+
+//Prompt user for a max number
+var max = prompt("Please type in a max number:");
+
+//validate using while loop
+while(isNaN(max) || max === ""){
+//Re-Prompt user
+    max = prompt("Please type in a max number:")
+}
+
+
+
+

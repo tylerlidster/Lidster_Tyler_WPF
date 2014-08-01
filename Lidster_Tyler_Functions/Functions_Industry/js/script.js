742277ae4a1de27aa872d3f9a0467c229eae83ef
tylerlidster <tyler.lidster@me.com>
2014-07-31 17:08:19 -0700

    Functions_Industry
    Week 4 assignment, variables - created code for assignment week 4
    Functions_Industry

---
+++ Lidster_Tyler_Functions/Functions_Industry/js/script.js
@@ -0,0 +1,20 @@
+//Tyler Lidster
+//WPF 1407 - Section 01
+// 7.31.14
+//Functions
+
+function tripCost(){
+    var nights = prompt("How many nights will you be need a hotel?");
+
+    var rooms = prompt("How many rooms will you need?");
+
+    var totalCost = (nights * rooms) * (100);
+    console.log(totalCost);
+
+}
+tripCost();
+
+
+
+
+

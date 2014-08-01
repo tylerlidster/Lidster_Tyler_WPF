efff8bf3ea19d59878155681e442b72d4d22455d
tylerlidster <tyler.lidster@me.com>
2014-07-31 20:08:30 -0700

    Function_Wacky
    Created week 4 assignment - Wacky - for calculating free throw
    percentage in basketball

--- Lidster_Tyler_Functions/Functions_Wacky/js/script.js
+++ Lidster_Tyler_Functions/Functions_Wacky/js/script.js
@@ -5,3 +5,15 @@
 //Function - wacky
 
 
+//FreeThrow Percentage
+
+function basketballFt(){
+    var totalFt = prompt("How many free throws did you shoot?");
+    var madeFt = prompt("How many free throws did you make?");
+    var freeThrowPct =( totalFt / madeFt );
+    console.log(freeThrowPct);
+
+    //return freeThrowPct;
+
+}
+basketballFt();
